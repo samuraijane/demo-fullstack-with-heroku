91b4ab183e4d921832e1b18d850a52f124469102
Matthew Day <matt@matthewday.net>
2022-07-11 14:06:10 +0200

    add two new contacts to seeder data

--- seeders/20210605131008-contact.js
+++ seeders/20210605131008-contact.js
@@ -2,19 +2,47 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Contacts', [{
-      address1: '123 Main Street',
-      address2: 'No. 2',
-      city: 'Springfield',
-      state: 'OR',
-      zipcode: '97403',
-      mobilephone: '541-555-1212',
-      email: 'anna@email.com',
-      firstName: 'Anna',
-      lastName: 'Anderson',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    await queryInterface.bulkInsert('Contacts', [
+      {
+        address1: '123 Main Street',
+        address2: 'No. 4',
+        city: 'Springfield',
+        state: 'OR',
+        zipcode: '97403',
+        mobilephone: '541-555-1212',
+        email: 'andrea@email.com',
+        firstName: 'Andrea',
+        lastName: 'Anderson',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        address1: '456 First Avenue',
+        address2: 'No. 7',
+        city: 'Fargo',
+        state: 'ND',
+        zipcode: '58102',
+        mobilephone: '701-555-1212',
+        email: 'billy@email.com',
+        firstName: 'Billy',
+        lastName: 'Bragg',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        address1: '789 Running Springs Way',
+        address2: '',
+        city: 'Macon',
+        state: 'GA',
+        zipcode: '31201',
+        mobilephone: '478-555-1212',
+        email: 'carlos@email.com',
+        firstName: 'Carlos',
+        lastName: 'Cunningham',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
