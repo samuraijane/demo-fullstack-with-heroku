52f88dd4e526b12c05e4877e6136ace28c3956de
Matthew Day <matt@matthewday.net>
2022-07-11 16:45:36 +0200

    change .error to .log

--- middleware/ensureAuth.js
+++ middleware/ensureAuth.js
@@ -1,7 +1,7 @@
 function ensureAuth(req, res, next) {
   const { auth } = req.query;
   if (!auth || auth !== process.env.AUTH) {
-    console.error('Unauthorized user');
+    console.log('!! Unauthorized user attempt');
     res.json({ message: 'You are not authorized to execute this request.', success: false });
     res.redirect('/');
   } else {
