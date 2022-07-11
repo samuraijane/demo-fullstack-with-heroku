7e9efceaf4a37d319593bfc44efd07c63d5ab63f
Matthew Day <matt@matthewday.net>
2022-07-11 10:43:44 +0200

    catch error when GET at /contacts fails

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/heartbeat', (req, res) => {
 });
 
 app.get('/contacts', async (req, res) => {
-  const contacts = await Contact.findAll();
+  const contacts = await Contact.findAll().catch(err => console.log('ERR', err));
   res.json(contacts);
 });
 
